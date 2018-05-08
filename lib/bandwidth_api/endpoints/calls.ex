80814ec07f8b4bb50e0d2dd7490441ab99a1f903
Tomas Koci <koci24@uga.edu>
2018-05-08 15:20:25 -0400

    separated call id from params in play_audio and update_call

--- lib/bandwidth_api/endpoints/calls.ex
+++ lib/bandwidth_api/endpoints/calls.ex
@@ -3,8 +3,8 @@ defmodule BandwidthApi.Endpoints.Calls do
   import BandwidthApi.Config, only: [user_id: 0]
 
 
-  def play_audio(%{"callId" => call_id} = params), do: Client.play_audio(partial_url(call_id) <> "/audio", params)
-  def update_call(%{"callId" => call_id} = params), do: Client.update_call(partial_url(call_id), params)
+  def play_audio(call_id, params), do: Client.play_audio(partial_url(call_id) <> "/audio", params)
+  def update_call(call_id, params), do: Client.update_call(partial_url(call_id), params)
 
 
   def partial_url(call_id), do: "users/#{user_id()}/calls/#{call_id}"
