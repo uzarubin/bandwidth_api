a0b9ada061863e02706ae0e79388bae22d89bc28
Tomas Koci <koci24@uga.edu>
2018-05-08 12:30:14 -0400

    added support for call endpoints

---
+++ lib/bandwidth_api/endpoints/calls.ex
@@ -0,0 +1,11 @@
+defmodule BandwidthApi.Endpoints.Calls do
+  alias BandwidthApi.Client
+  import BandwdithApi.Config, only: [usert_id: 0]
+
+
+  def play_audio(%{"callId" => call_id} = params), do: Client.play_audio(partial_url(call_id) <> "/audio", params)
+  def update_call(%{"callId" => call_id} = params), do: Client.update_call(partial_url(call_id), params)
+
+
+  def partial_url(call_id), do: "users/#{user_id()}/calls/#{call_id}"
+end
